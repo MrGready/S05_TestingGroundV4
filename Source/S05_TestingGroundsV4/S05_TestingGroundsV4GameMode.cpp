f5087ade46fe4c44c67f2611e74ff1fac8c8bd83
RedSigma-PC\RedSigma <rougesigmaready@gmail.com>
2018-09-09 22:29:37 -0700

    TG13_Cleaning Up the Code and Deleting Unneccessary Files

--- Source/S05_TestingGroundsV4/S05_TestingGroundsV4GameMode.cpp
+++ Source/S05_TestingGroundsV4/S05_TestingGroundsV4GameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "S05_TestingGroundsV4GameMode.h"
 #include "S05_TestingGroundsV4HUD.h"
-#include "S05_TestingGroundsV4Character.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AS05_TestingGroundsV4GameMode::AS05_TestingGroundsV4GameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
